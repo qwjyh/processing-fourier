b708a146840140f92da87ace8abeb9c0ded179e5
qwjyh <urataw421@gmail.com>
2022-12-07 02:31:31 +0900

    (WIP) FT test script with function plotting & integration

---
+++ fourier.jl
@@ -0,0 +1,35 @@
+using GLMakie
+
+# integration
+function integrate(f::Function, range::Tuple{Real, Real}, n::Integer)
+    # type check
+    @assert typeof(f(range[1] + rand() * (range[2] - range[1]))) <: Real
+    
+    Δx = (range[2] - range[1]) / n
+    sum = 0.0
+    for x in LinRange(range[1], range[2], n)
+        sum += Δx * f(x)
+    end
+    return sum
+end
+
+print(integrate(sin, (0, 2π), 1000))
+
+
+function parametric_square(t::Real)
+    @assert 0 ≤ t ≤ 2π
+    if t < π/4
+        return (1                 , t / (4/π)      )
+    elseif t < 3π/4
+        return ((π/2 - t) * (4/π) , 1              )
+    elseif t < 5π/4
+        return (-1                , (π - t) * (4/π))
+    elseif t < 7π/4
+        return ((t - 3π/2) * (4/π), -1             )
+    else
+        return (1                 , (t - 2π) * (4/π))
+    end
+end
+
+θs = LinRange(0, 2π, 99)
+lines(parametric_square.(θs) .|> x -> x[1], parametric_square.(θs) .|> x -> x[2])
